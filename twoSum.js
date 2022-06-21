9631796d7972fc1edac4155831ddbfa9e27a1033
Mohfez Rahman <mohfezrahman@gmail.com>
2022-06-21 11:52:15 -0400

    Added two solutions

---
+++ twoSum.js
@@ -0,0 +1,10 @@
+var twoSum = function(nums, target) {
+    var hash = {};
+    var len = nums.length;
+    for (var i = 0; i < len; i++) {
+      if (nums[i] in hash) return [hash[nums[i]], i];
+      hash[target - nums[i]] = i
+    }
+    return [-1, -1];
+  };
+  console.log(twoSum([2,7,11,15],9))
