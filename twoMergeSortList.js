addab54fe156c6b2ab47cf59043107a8d7912758
Mohfez Rahman <mohfezrahman@gmail.com>
2022-06-21 12:39:17 -0400

    "updated"

--- twoMergeSortList.js
+++ twoMergeSortList.js
@@ -1,16 +1,20 @@
+
 var mergeTwoLists = function (list1, list2) {
-    const head = new ListNode();
-    let node = head;
-    while (list1 && list2) {
-      if (list1.val > list2.val) {
-        node.next = list2;
-        list2 = list2.next;
-      } else {
-        node.next = list1;
-        list1 = list1.next;
-      }
-      node = node.next;
+  const head = new ListNode();
+  let node = head;
+  while (list1 && list2) {
+    if (list1.val > list2.val) {
+      node.next = list2;
+      list2 = list2.next;
+    } else {
+      node.next = list1;
+      list1 = list1.next;
     }
-    node.next = list1 || list2;
-    return head.next;
-  };
+    node = node.next;
+  }
+  node.next = list1 || list2;
+  return head.next;
+};
+
+
+
